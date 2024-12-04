7b70bc00e8072104189a63dddb0c3eeee423e788
Brock Herion <brockheriondev@gmail.com>
2024-12-04 16:37:37 -0600

    More day 2 work

--- apps/day_2/lib/utils.ex
+++ apps/day_2/lib/utils.ex
@@ -8,43 +8,23 @@ defmodule Day2.Utils do
   end
 
   def has_bad_level?(list, trend) do
-    conforms_to_order = conforms_to_order?(list)
+    list_len = length(list)
 
-    if not conforms_to_order do
-      true
-    else
-      list_len = length(list)
-
-      list
-      |> Enum.with_index()
-      |> Enum.any?(fn {current, index} ->
-        if index < list_len - 1 do
-          next = Enum.at(list, index + 1)
-          not valid_adjacent_pairs?(current, next, trend)
-        else
-          false
-        end
-      end)
-    end
+    list
+    |> Enum.with_index()
+    |> Enum.any?(fn {current, index} ->
+      if index < list_len - 1 do
+        next = Enum.at(list, index + 1)
+        not valid_adjacent_pairs?(current, next, trend)
+      else
+        false
+      end
+    end)
   end
 
   def valid_adjacent_pairs?(current, next, trend) do
     abs(current - next) <= 3 and current != next and
       ((trend == :inc and current < next) or (trend == :dec and current > next))
-  end
-
-  def conforms_to_order?(list) do
-    is_increasing?(list) or is_decreasing?(list)
-  end
-
-  def is_increasing?(list) do
-    Enum.zip(list, tl(list))
-    |> Enum.all?(fn {a, b} -> a <= b end)
-  end
-
-  def is_decreasing?(list) do
-    Enum.zip(list, tl(list))
-    |> Enum.all?(fn {a, b} -> a >= b end)
   end
 
   def get_initial_trend(list) do
